16a52fc9cb993d7c0083330cdce475c47efd43cd
prasis73 <prasis73@gmail.com>
2019-11-11 09:38:57 +0545

    first class

---
+++ intern.py
@@ -0,0 +1,23 @@
+print("INtern classes")
+'''a=3
+b=5
+c=a+b
+print(c)
+print("a"+"b")
+print("h\ne\nl\nl\no")
+name="Prasis Rijal"
+print("My name is",name)'''
+first_name=input("enter first name")
+last_name=input("enter last name")
+age=input("enter age")
+address=input("enter address")
+naam=first_name+' ' + last_name
+print("Name:",naam,"\n  Age:",age,"\nAddress:",address)
+first=int(input("enter first number"))
+second=int(input("enter second number"))
+add=first+second
+sub=first-second
+mul=first*second
+div=first/second
+print("addition= %d \n subtraction= %d \nmultiplication= %d \n division= %d" %(add,sub,mul,div))
+#print(type(c))
