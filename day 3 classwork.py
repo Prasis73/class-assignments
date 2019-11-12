32d5e2389847455780249105684a94b5c42c392f
prasis73 <prasis73@gmail.com>
2019-11-12 22:00:08 +0545

    assignment

--- day 3 classwork.py
+++ day 3 classwork.py
@@ -4,11 +4,32 @@ social=int(input("marks of social"))
 nepali=int(input("marks of nepali"))
 computer=int(input("marks of computer"))
 list=[math,science,social,nepali,computer]
-print(list)
-total=math+science+social+nepali+computer
+print(list)'''
+'''total=math+science+social+nepali+computer
 print("total marks=",total)
 
 print(sum(list))'''
 
+#take string and count any string
+'''name=(input("enter string"))
+#name.upper()
+no=input("enter string to be counted")
+a=no.upper()
+b=no.lower()
+c=name.count(a)
+d=name.count(b)
+e=c+d
+print(e)
+#print(name.count(no))'''
+
+'''print("maximum number",max(list))
+print("minimum number",min(list))'''
+
+#print(name.lower())    #for lowercase
+#print(name.upper())    #for uppercase
+
+#Replacing
 name=(input("enter string"))
-print(name.count(input("enter string to be counted")))
+c=name.replace(input("enter string to be replaced:"),input("replaced by:"))
+print(c)
+
