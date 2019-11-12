a7fb7ede154b34dd00dcc16357f2f4edeba236fd
prasis73 <prasis73@gmail.com>
2019-11-12 09:34:40 +0545

    class work

---
+++ day 3 classwork.py
@@ -0,0 +1,14 @@
+'''math=int(input("marks of math"))
+science=int(input("marks of science"))
+social=int(input("marks of social"))
+nepali=int(input("marks of nepali"))
+computer=int(input("marks of computer"))
+list=[math,science,social,nepali,computer]
+print(list)
+total=math+science+social+nepali+computer
+print("total marks=",total)
+
+print(sum(list))'''
+
+name=(input("enter string"))
+print(name.count(input("enter string to be counted")))
