f1b42a8ac51655df0ffc8079a2a3dfde8c9d59a9
prasis73 <prasis73@gmail.com>
2019-11-12 08:56:23 +0545

    Third day

--- day 3.py
+++ day 3.py
@@ -1,1 +1,31 @@
-print("lol")
+print("hello")
+list=[1,2,4,5,7,9,6,8]
+#APPEND(adds the value in list)
+'''list.append("p")
+print(list)'''
+
+#POP(removes the data from given index)
+'''list.pop(2)
+print(list)'''
+
+#SORT(sorts in ascending order)
+list.sort()    #arranges in ascending order
+print(list)
+list.sort(reverse=True)   #arranges in descending order
+
+#COUNT(counts the given value)
+print(list.count(1))
+
+#Extend (extends any two lists)
+list1=[21,34,25,32]
+list.extend(list1)
+print(list)
+
+
+name="My name is Prasis Rijal"
+print(name.count("a"))    #counts number of a
+print(name[1:5])  #prints value from index 0-4
+print(name[0:9:2])   #jumps two steps
+
+print(name)
+
